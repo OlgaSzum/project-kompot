d74f4cbdac8a4d9699fa47943ee0407cec25f78c
OlgaSzum <126341342+OlgaSzum@users.noreply.github.com>
2023-09-11 19:32:54 +0000

    Deploying to gh-pages from @ OlgaSzum/project-kompot@9f9eaf23111bd896ad8dc5ad6f13bde45b2428c8 🚀

--- index.7f3551a0.js
+++
@@ -1,3 +0,0 @@
-(()=>{let o={openModalBtnProduct:document.querySelector("[product-modal-open]"),closeModalBtnProduct:document.querySelector("[product-modal-close]"),modal:document.querySelector("[product-modal]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtnProduct.addEventListener("click",e),o.closeModalBtnProduct.addEventListener("click",e)})();//# sourceMappingURL=index.7f3551a0.js.map
-
-//# sourceMappingURL=index.7f3551a0.js.map
