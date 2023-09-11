9195e79a9da88ef032de78078bb7f68c7a22315e
OlgaSzum <126341342+OlgaSzum@users.noreply.github.com>
2023-09-11 19:48:39 +0000

    Deploying to gh-pages from @ OlgaSzum/project-kompot@0e2605c866f08038b0c82c5e599374705bc0e3b5 🚀

---
+++ index.67c6d872.js
@@ -0,0 +1,8 @@
+(()=>{let e={openModalBtnProduct:document.querySelector("[product-modal-open]"),closeModalBtnProduct:document.querySelector("[product-modal-close]"),modal:document.querySelector("[product-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtnProduct.addEventListener("click",o),e.closeModalBtnProduct.addEventListener("click",o)})(),new Swiper(".swiper",{// Optional parameters
+direction:"horizontal",loop:!0,// If we need pagination
+pagination:{el:".swiper-pagination",clickable:!0},// Navigation arrows
+navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},// And if we need scrollbar
+scrollbar:{el:".swiper-scrollbar"}});// import './js/modal-buy-now';
+//# sourceMappingURL=index.67c6d872.js.map
+
+//# sourceMappingURL=index.67c6d872.js.map
--- index.e9a44c53.js
+++
@@ -1,3 +0,0 @@
-new Swiper(".mySwiper",{slidesPerView:1,grabCursor:!0,loop:!0,pagination:{el:".swiper-pagination",clickable:!0}});//# sourceMappingURL=index.e9a44c53.js.map
-
-//# sourceMappingURL=index.e9a44c53.js.map
